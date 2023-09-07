55162b8afed5ff4abc7e95d8a18aab912d4b44ac
rmcarlosi <144172310+rmcarlosi@users.noreply.github.com>
2023-09-07 10:11:51 -0300

    Update streamlit_app.py
    add a recipe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title ("My first streamlit app")
+streamlit.title ("My parents new healthy dinner")
+streamlit.header('Breakfast menu')
+stramliet.text('Omega 3 and blueberries oatmeal')
+stramliet.text('Kale, spinach and rocket smoothie')
+stramliet.text('Hard Boiled free range egg')
